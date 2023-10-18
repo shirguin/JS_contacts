1bf4b78084b3f8cdbad38fe3e00272b87e41a348
shirguin <shirguin@yandex.ru>
2023-10-18 07:06:34 +0300

    made the creation and output of contacts

--- addContact.js
+++ addContact.js
@@ -1,5 +1,36 @@
-const backButtonEl = document.querySelector('#back');
+const addContactButtonEl = document.querySelector(".saveContact");
+const exitButtonEl = document.querySelector(".exit");
+const nameInputEl = document.querySelector("#name");
+const surnameInputEl = document.querySelector("#surname");
+const positionInputEl = document.querySelector("#position");
+const firmNameInputEl = document.querySelector("#firmName");
+const emailInputEl = document.querySelector("#email");
+const telephoneInputEl = document.querySelector("#telephone");
 
-addButtonEl.addEventListener('click', ()=>{
-    window.location.replace("./index.html");
+addContactButtonEl.addEventListener("click", () => {
+  let newContact = {
+    name: nameInputEl.value,
+    surname: surnameInputEl.value,
+    position: positionInputEl.value,
+    firmName: firmNameInputEl.value,
+    email: emailInputEl.value,
+    telephone: telephoneInputEl.value,
+  };
+
+  console.log(newContact);
+
+  let listContacts = [];
+
+  if (localStorage.getItem("contacts") !== null) {
+    listContacts = JSON.parse(localStorage.getItem("contacts"));
+  }
+
+  listContacts.push(newContact);
+  localStorage.setItem("contacts", JSON.stringify(listContacts));
+  window.location.replace("./index.html");
+});
+
+//Переход на главную
+exitButtonEl.addEventListener("click", () => {
+  window.location.replace("./index.html");
 });
--- script.js
+++ script.js
@@ -1,5 +1,31 @@
-const addButtonEl = document.querySelector('#addContact');
+let listContacts = [];
 
-addButtonEl.addEventListener('click', ()=>{
-    window.location.replace("./addContact.html");
+//Получаем список контактов из localStorage
+if (localStorage.getItem("contacts") !== null) {
+  listContacts = JSON.parse(localStorage.getItem("contacts"));
+}
+
+const divContactsEl = document.querySelector(".contacts");
+
+//Выводим контакты на экран
+listContacts.forEach((contact) => {
+  const newContactEl = document.createElement("div");
+  newContactEl.className = "contact__item";
+  newContactEl.innerHTML = `
+        <div class="column contact__name">${contact.name}</div>
+        <div class="column contact__surname">${contact.surname}</div>
+        <div class="column contact__position">${contact.position}</div>
+        <div class="column contact__firmName">${contact.firmName}</div>
+        <div class="column contact__email">${contact.email}</div>
+        <div class="column contact__telephone">${contact.telephone}</div>
+        <img class="icon" src="./edit.svg" alt="iconedit" />
+        <img class="icon" src="./delete.svg" alt="iconedit" />
+    `;
+  divContactsEl.append(newContactEl);
+});
+
+const addButtonEl = document.querySelector(".addContact");
+
+addButtonEl.addEventListener("click", () => {
+  window.location.replace("./addContact.html");
 });
