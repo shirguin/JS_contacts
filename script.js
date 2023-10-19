0f87b80e48b4e834edee949f6c9fb30af57f5adf
shirguin <shirguin@yandex.ru>
2023-10-19 16:13:24 +0300

    finished editing the contact

--- script.js
+++ script.js
@@ -7,6 +7,9 @@ if (localStorage.getItem("contacts") !== null) {
 
 const divContactsEl = document.querySelector(".contacts");
 
+//Добавляем заголовки с фильтрами.
+const headersContactEl = ``;
+
 //Выводим контакты на экран
 listContacts.forEach((contact) => {
   const newContactEl = document.createElement("div");
@@ -28,23 +31,6 @@ listContacts.forEach((contact) => {
         </div>
     `;
   divContactsEl.append(newContactEl);
-});
-
-//Редактирование контакта
-const editButtonEls = document.querySelectorAll(".icon__edit");
-
-editButtonEls.forEach((button) => {
-  button.addEventListener("click", (e) => {
-    const divItemEl = e.target.closest(".contact__item");
-
-    let name = divItemEl.querySelector(".contact__name").textContent;
-    let surname = divItemEl.querySelector(".contact__surname").textContent;
-    let position = divItemEl.querySelector(".contact__position").textContent;
-    let firmName = divItemEl.querySelector(".contact__firmName").textContent;
-    let email = divItemEl.querySelector(".contact__email").textContent;
-    let telephone = divItemEl.querySelector(".contact__telephone").textContent;
-    console.log("edit");
-  });
 });
 
 //Удаление контакта
@@ -86,9 +72,9 @@ function getIndex(divItemEl) {
   }
 }
 
-//Модальное окно добавления контакта
-const addButtonEl = document.querySelector(".icon__add");
+//Модальное окно
 const modalEl = document.querySelector(".modal");
+const modalContentEl = document.querySelector(".modal__content");
 
 modalEl.style.cssText = `
   display: flex;
@@ -98,16 +84,9 @@ modalEl.style.cssText = `
 `;
 
 //Закрытие модального окна
-const closeModal = (event) => {
-  const target = event.target;
-
-  if (target === modalEl || target.closest(".modal__close")) {
-    modalEl.style.opacity = 0;
-
-    setTimeout(() => {
-      modalEl.style.visibility = "hidden";
-    }, 300);
-  }
+const closeModal = () => {
+  modalEl.style.opacity = 0;
+  modalEl.style.visibility = "hidden";
 };
 
 //Открытие модального окна
@@ -116,43 +95,198 @@ const openModal = () => {
   modalEl.style.opacity = 1;
 };
 
-addButtonEl.addEventListener("click", openModal);
-modalEl.addEventListener("click", closeModal);
+//Очистка модального окна
+const clearModal = () => {
+  modalContentEl.innerHTML = "";
+};
 
 //Добавление контакта
-const addContactButtonEl = document.querySelector(".saveContact");
-const exitButtonEl = document.querySelector(".exit");
-const nameInputEl = document.querySelector("#name");
-const surnameInputEl = document.querySelector("#surname");
-const positionInputEl = document.querySelector("#position");
-const firmNameInputEl = document.querySelector("#firmName");
-const emailInputEl = document.querySelector("#email");
-const telephoneInputEl = document.querySelector("#telephone");
+const addButtonEl = document.querySelector(".icon__add");
+addButtonEl.addEventListener("click", () => {
+  //Вставляем в модальное окно форму для ввода нового контакта
+  const html = `
+          <h1 class="title">Добавление контакта</h1>
+          <div class="forma">
+            <div class="form__item">
+              <p class="label">Имя</p>
+              <input class="textInput" type="text" id="name" />
+            </div>
 
-addContactButtonEl.addEventListener("click", () => {
-  let newContact = {
-    name: nameInputEl.value,
-    surname: surnameInputEl.value,
-    position: positionInputEl.value,
-    firmName: firmNameInputEl.value,
-    email: emailInputEl.value,
-    telephone: telephoneInputEl.value,
-  };
+            <div class="form__item">
+              <p class="label">Фамилия</p>
+              <input class="textInput" type="text" id="surname" />
+            </div>
 
-  console.log(newContact);
+            <div class="form__item">
+              <p class="label">Должность</p>
+              <input class="textInput" type="text" id="position" />
+            </div>
 
-  let listContacts = [];
+            <div class="form__item">
+              <p class="label">Фирма</p>
+              <input class="textInput" type="text" id="firmName" />
+            </div>
 
-  if (localStorage.getItem("contacts") !== null) {
-    listContacts = JSON.parse(localStorage.getItem("contacts"));
-  }
+            <div class="form__item">
+              <p class="label">Email</p>
+              <input class="textInput" type="email" id="email" />
+            </div>
 
-  listContacts.push(newContact);
-  localStorage.setItem("contacts", JSON.stringify(listContacts));
-  window.location.replace("./index.html");
+            <div class="form__item">
+              <lp class="label">Телефон</lp>
+              <input class="textInput" type="text" id="telephone" />
+            </div>
+
+            <div class="blockButtons">
+              <button class="btn saveContact">Добавить</button>
+              <button class="btn exit">Выход</button>
+            </div>
+          </div>
+  `;
+  modalContentEl.innerHTML = html;
+
+  //открываем модальное окно
+  openModal();
+
+  //Добавление контакта
+  const addContactButtonEl = document.querySelector(".saveContact");
+  const exitButtonEl = document.querySelector(".exit");
+  const nameInputEl = document.querySelector("#name");
+  const surnameInputEl = document.querySelector("#surname");
+  const positionInputEl = document.querySelector("#position");
+  const firmNameInputEl = document.querySelector("#firmName");
+  const emailInputEl = document.querySelector("#email");
+  const telephoneInputEl = document.querySelector("#telephone");
+
+  addContactButtonEl.addEventListener("click", () => {
+    let newContact = {
+      name: nameInputEl.value,
+      surname: surnameInputEl.value,
+      position: positionInputEl.value,
+      firmName: firmNameInputEl.value,
+      email: emailInputEl.value,
+      telephone: telephoneInputEl.value,
+    };
+
+    let listContacts = [];
+
+    if (localStorage.getItem("contacts") !== null) {
+      listContacts = JSON.parse(localStorage.getItem("contacts"));
+    }
+
+    listContacts.push(newContact);
+    localStorage.setItem("contacts", JSON.stringify(listContacts));
+
+    //Очищаем модальное окно
+    clearModal();
+
+    //Обновляем страницу
+    window.location.replace("./index.html");
+  });
+
+  //Выход из формы модального окна
+  exitButtonEl.addEventListener("click", () => {
+    clearModal();
+    closeModal();
+  });
 });
 
-//Выход из формы модального окна
-exitButtonEl.addEventListener("click", () => {
-  closeModal();
+//Редактирование контакта
+const editButtonEls = document.querySelectorAll(".icon__edit");
+
+editButtonEls.forEach((button) => {
+  button.addEventListener("click", (e) => {
+    const divItemEl = e.target.closest(".contact__item");
+    let indexEditContact = getIndex(divItemEl);
+    const editContact = listContacts[indexEditContact];
+
+    const html = `
+          <h1 class="title">Редактирование контакта</h1>
+          <div class="forma">
+            <div class="form__item">
+              <p class="label">Имя</p>
+              <input class="textInput" type="text" id="name" />
+            </div>
+
+            <div class="form__item">
+              <p class="label">Фамилия</p>
+              <input class="textInput" type="text" id="surname" />
+            </div>
+
+            <div class="form__item">
+              <p class="label">Должность</p>
+              <input class="textInput" type="text" id="position" />
+            </div>
+
+            <div class="form__item">
+              <p class="label">Фирма</p>
+              <input class="textInput" type="text" id="firmName" />
+            </div>
+
+            <div class="form__item">
+              <p class="label">Email</p>
+              <input class="textInput" type="email" id="email" />
+            </div>
+
+            <div class="form__item">
+              <lp class="label">Телефон</lp>
+              <input class="textInput" type="text" id="telephone" />
+            </div>
+
+            <div class="blockButtons">
+              <button class="btn saveContact">Сохранить</button>
+              <button class="btn exit">Выход</button>
+            </div>
+          </div>
+  `;
+    modalContentEl.innerHTML = html;
+
+    //Вставляем данные в форму
+    const saveContactButtonEl = document.querySelector(".saveContact");
+    const exitButtonEl = document.querySelector(".exit");
+    const nameInputEl = document.querySelector("#name");
+    const surnameInputEl = document.querySelector("#surname");
+    const positionInputEl = document.querySelector("#position");
+    const firmNameInputEl = document.querySelector("#firmName");
+    const emailInputEl = document.querySelector("#email");
+    const telephoneInputEl = document.querySelector("#telephone");
+
+    nameInputEl.value = editContact.name;
+    surnameInputEl.value = editContact.surname;
+    positionInputEl.value = editContact.position;
+    firmNameInputEl.value = editContact.firmName;
+    emailInputEl.value = editContact.email;
+    telephoneInputEl.value = editContact.telephone;
+
+    //открываем модальное окно
+    openModal();
+
+    //сохраняем отредактированные данные
+    saveContactButtonEl.addEventListener("click", () => {
+      let newContact = {
+        name: nameInputEl.value,
+        surname: surnameInputEl.value,
+        position: positionInputEl.value,
+        firmName: firmNameInputEl.value,
+        email: emailInputEl.value,
+        telephone: telephoneInputEl.value,
+      };
+
+      listContacts[indexEditContact] = newContact;
+
+      localStorage.setItem("contacts", JSON.stringify(listContacts)); 
+
+      //Очищаем модальное окно
+      clearModal();
+
+      //Обновляем страницу
+      window.location.replace("./index.html");
+    });
+
+    //Выход из формы модального окна
+    exitButtonEl.addEventListener("click", () => {
+      clearModal();
+      closeModal();
+    });
+  });
 });
